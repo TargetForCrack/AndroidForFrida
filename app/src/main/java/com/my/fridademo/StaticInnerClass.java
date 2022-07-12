a4bed8f0e3f0b1a0c6e42e973b5c3218700c316a
dio <dio@os.jobs8.cn>
2022-07-12 20:44:29 +0800

    some notes

--- app/src/main/java/com/my/fridademo/StaticInnerClass.java
+++ app/src/main/java/com/my/fridademo/StaticInnerClass.java
@@ -4,6 +4,7 @@ package com.my.fridademo;
 public class StaticInnerClass {
     int num = 88;
 
+    // 静态内部类和非静态内部类最大的区别是：非静态内部类编译后隐式保存着外部类的引用（就算外部类对象没用了也GC不掉）,但是静态内部类没有
     static class Inner {
         int num = 99;
 
